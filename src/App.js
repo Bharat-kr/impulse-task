c72dd8744882f987cecec77662c21e3238c7efec
Bharat Kumar <kumarbharat661@gmail.com>
2022-05-30 16:27:02 +0530

    all assests added with navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navabar/Navbar";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navabar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return <nav>Navbar</nav>;
+};
+
+export default Navbar;
